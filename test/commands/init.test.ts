6e222ea59cabea9276a23a9545672b7a43039def
jmbeach <jaredbeachdesign@gmail.com>
2020-01-20 14:14:23 -0600

    adds hook test

--- test/commands/init.test.ts
+++ test/commands/init.test.ts
@@ -1,9 +1,8 @@
-import {expect, test} from '@oclif/test'
+import {test} from '@oclif/test'
 
 describe('hooks', () => {
   test
   .stdout()
-  .hook('init', {id: 'mycommand'})
-  .do(output => expect(output.stdout).to.contain('example hook running mycommand'))
-  .it('shows a message')
+  .hook('init', {id: 'help', argv: ['conf']})
+  .it('doesn\'t error')
 })
