d4aab061c1b4835a03ff2011fb00282645c31230
jmbeach <jaredbeachdesign@gmail.com>
2020-01-20 14:17:05 -0600

    adds test for plans --enabled

--- test/commands/plans.test.ts
+++ test/commands/plans.test.ts
@@ -19,11 +19,31 @@ describe('plans', () => {
       ],
     },
   }
+
+  const expectedEnabled = {
+    plans: {
+      plan: [
+        {
+          name: 'plan 1 - enabled',
+          enabled: true,
+        },
+      ],
+    },
+  }
+
   test
   .nock(config.baseUrl, api => api.get('/rest/api/latest/plan.json').reply(200, mock))
   .stdout()
   .command(['plans'])
   .it('runs plans', ctx => {
     expect(ctx.stdout).to.contain(stringify(mock, null, config.tabCount))
+  })
+
+  test
+  .nock(config.baseUrl, api => api.get('/rest/api/latest/plan.json').reply(200, mock))
+  .stdout()
+  .command(['plans', '--enabled'])
+  .it('runs plans --enabled', ctx => {
+    expect(ctx.stdout).to.contain(stringify(expectedEnabled, null, config.tabCount))
   })
 })
