223642b694f8b3c3164d6541ef61f411da5d3844
jmbeach <jaredbeachdesign@gmail.com>
2020-01-20 19:18:44 -0600

    replace fake command with proxy

--- test/test-helper.ts
+++ test/test-helper.ts
@@ -1,13 +1,39 @@
 import * as proxyquire from 'proxyquire'
-import FakeCommand from './fake-command'
+function fakeConfigurationParserFactory(url: string, tabCount: number) {
+  return class FakeConfigurationParser {
+    static parse() {
+      return {
+        bambooUrl: url,
+        errorMessages: [],
+        password: 'test1234',
+        tabCount: tabCount,
+        username: 'test',
+      }
+    }
+  }
+}
+
 export default class TestHelper {
   static baseUrl = 'http://localhost:54663'
 
   static tabCount = 4
 
   static async getCommand(commandName: string, argv?: Array<any>) {
-    const Command = proxyquire.noCallThru()(`../src/commands/${commandName}`, {'../bamboo-client-command': {default: FakeCommand}}).default
+    const BambooClientCommandClone = proxyquire('../src/bamboo-client-command', {
+      './configuration-parser': {default: fakeConfigurationParserFactory(TestHelper.baseUrl, TestHelper.tabCount)},
+    }).default
+    const Command = proxyquire.noCallThru()(`../src/commands/${commandName}`, {'../bamboo-client-command': {default: BambooClientCommandClone}}).default
     const cmd = new Command()
+    cmd.log = (message: any) => {
+      if (typeof message !== 'string') {
+        return
+      }
+
+      cmd.stdout.push(message)
+    }
+
+    cmd.stderr = []
+    cmd.stdout = []
     cmd.tabCount = TestHelper.tabCount
     cmd.config = {
       debug: false,
